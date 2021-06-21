04a74fc416bfe459cdfd4a8129d04083647a6838
megi-j <82594147+megi-j@users.noreply.github.com>
2021-06-21 16:05:03 +0400

    change global var to local

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 function createCalculator(){
-    let numbers = document.querySelectorAll('.N-button');
-    let operators = document.querySelectorAll('.oparator');
-    let equal = document.querySelector('.do');
-    let clear = document.querySelector('.clear');
-    let result = document.querySelector('.result-desk');
-
+    let result = document.getElementsByClassName('.result-desk');
 
     function numbersOnTheDesk(){
+        let numbers = document.querySelectorAll('.N-button');
         for(let i = 0; i < numbers.length; i++){
             numbers[i].addEventListener('click', function(){
                 let numbersValue = numbers[i].value;
@@ -25,8 +21,8 @@ function createCalculator(){
             result.innerText = "";
         }         
     }
-    
     function operatorsOntheDesk(){
+        let operators = document.querySelectorAll('.oparator');
         for(let i = 0; i < operators.length; i++){
             operators[i].addEventListener('click', function(){
                 let operatorsValue = operators[i].value;
@@ -37,12 +33,14 @@ function createCalculator(){
         } 
     }
     function clearText(){
+        let clear = document.getElementsByClassName('.clear');
         clear.addEventListener('click', function(){
             result.innerText = "";
         })
     }
 
     function calculate() {
+        let equal = document.getElementsByClassName('.do');
         equal.addEventListener('click', function(){
             result.innerText = eval(result.innerText);
         })
